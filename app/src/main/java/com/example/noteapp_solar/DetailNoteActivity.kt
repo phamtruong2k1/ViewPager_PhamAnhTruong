40a4734fb28da9843768ac0540d6564501dc05f8
Phạm Ánh Trường <61191253+phamtruong2k1@users.noreply.github.com>
2023-03-02 09:39:08 +0700

    first commit

---
+++ app/src/main/java/com/example/noteapp_solar/DetailNoteActivity.kt
@@ -0,0 +1,34 @@
+package com.example.noteapp_solar
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.layout_item_detail.*
+
+class DetailNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_item_detail)
+        val oldTitle = intent.getStringExtra("title") ?: ""
+        val oldTimeNote = intent.getStringExtra("timeNote") ?: ""
+        val oldContent = intent.getStringExtra("content") ?: ""
+        val oldNote = Note(oldTitle, oldTimeNote, oldContent)
+        inputDetailTitle.setText(oldTitle)
+        inputDetailTimeNote.setText(oldTimeNote)
+        inputDetailContentNote.setText(oldContent)
+        btnUpdateNote.setOnClickListener {
+            val newTitle = inputDetailTitle.text.toString()
+            val newTimeNote = inputDetailTimeNote.text.toString()
+            val newContent = inputDetailContentNote.text.toString()
+            val newNote = Note(newTitle, newTimeNote, newContent)
+            val status = NoteDatabase(this).updateNote(oldNote, newNote)
+            startActivity(Intent(this, NoteHomeScreen::class.java))
+            finish()
+        }
+        btnDeleteNote.setOnClickListener {
+            val status = NoteDatabase(this).deleteNote(oldNote)
+            startActivity(Intent(this, NoteHomeScreen::class.java))
+            finish()
+        }
+    }
+}
